6724f052061db4a938f43b69a028eb8e8a49588f
mohammed-safaan <alisafaan2015@gmail.com>
2022-08-14 00:19:26 +0200

    modified watchlist page , signup page

--- client/src/pages/watchlist/watchlist.jsx
+++ client/src/pages/watchlist/watchlist.jsx
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from "react";
 import Navbar from "../../components/navbar/Navbar";
 import Footer from "../../components/containers/Footer";
 import "./watchlist.scss";
-import Footer from "../../components/Footer/Footer";
 import WatchlistItem from "./WatchlistItem";
 
 const Watchlist = () => {
