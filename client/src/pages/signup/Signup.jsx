90feed432a8ca693d4802c3f36647874bd4494a2
mohammed-safaan <alisafaan2015@gmail.com>
2022-08-13 21:57:02 +0200

    modified register , signup and login pages

--- client/src/pages/signup/Signup.jsx
+++ client/src/pages/signup/Signup.jsx
@@ -1,17 +1,17 @@
-import { Link, useNavigate } from 'react-router-dom';
-import './signup.scss';
-import Footer from '../../components/containers/Footer';
-import joi from 'joi';
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom'
+import './signup.scss'
+import Footer from '../../components/containers/Footer'
+import joi from 'joi'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Login = () => {
-  const [errorValidation, setErrorValidation] = useState([]);
-  const [errorRegister, setErrorRegister] = useState('');
-  const [confirm, setConfirm] = useState('');
-  const [errorValidationName, setErrorValidationName] = useState('');
-  const [errorValidationEmail, setErrorValidationEmail] = useState('');
-  const [errorValidationPassword, setErrorValidationPassword] = useState('');
+  const [errorValidation, setErrorValidation] = useState([])
+  const [errorRegister, setErrorRegister] = useState('')
+  const [confirm, setConfirm] = useState('')
+  const [errorValidationName, setErrorValidationName] = useState('')
+  const [errorValidationEmail, setErrorValidationEmail] = useState('')
+  const [errorValidationPassword, setErrorValidationPassword] = useState('')
 
   // ****************
 
@@ -19,74 +19,74 @@ const Login = () => {
     username: '',
     email: '',
     password: '',
-  });
+  })
 
-  const { username, email, password } = formData;
+  const { username, email, password } = formData
 
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   // handle data in inputs
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
       [e.target.name]: e.target.value,
-    }));
-  };
+    }))
+  }
 
   // handle submition
   const onSubmit = async (e) => {
-    e.preventDefault();
-    let result = validation(formData);
+    e.preventDefault()
+    let result = validation(formData)
     if (result.error) {
-      console.log(result.error.details);
+      //setErrorValidation(result.error.details);
       if (result.error.details[0].path[0] === 'username') {
-        setErrorValidationName(result.error.details[0].message);
+        setErrorValidationName(result.error.details[0].message)
       } else {
-        setErrorValidationName('');
+        setErrorValidationName('')
       }
       if (result.error.details[0].path[0] === 'email') {
-        setErrorValidationEmail(result.error.details[0].message);
+        setErrorValidationEmail(result.error.details[0].message)
       } else {
-        setErrorValidationEmail('');
+        setErrorValidationEmail('')
       }
       if (result.error.details[0].path[0] === 'password') {
-        setErrorValidationPassword(result.error.details[0].message);
+        setErrorValidationPassword(result.error.details[0].message)
       } else {
-        setErrorValidationPassword('');
+        setErrorValidationPassword('')
       }
-      console.log(result.error.details[0].path[0] === 'username');
+      console.log(result.error.details[0].path[0] === 'username')
     } else {
-      setErrorValidation([]);
+      setErrorValidation([])
 
       const userData = {
         username,
         email,
         password,
-      };
+      }
 
       const { data } = await axios.post(
         `http://localhost:8800/api/auth/register`,
         userData
-      );
+      )
       if (data.message === 'Done') {
-        setErrorValidationName('');
-        setErrorValidationEmail('');
-        setErrorValidationPassword('');
-        setErrorRegister('');
-        setConfirm('please confirm your email');
+        setErrorValidationName('')
+        setErrorValidationEmail('')
+        setErrorValidationPassword('')
+        setErrorRegister('')
+        setConfirm('please confirm your email')
         setTimeout(() => {
-          navigate('/login');
-        }, 2000);
+          navigate('/login')
+        }, 2000)
       } else {
-        console.log(555);
-        setConfirm('');
-        setErrorRegister(data.message);
-        setErrorValidationName('');
-        setErrorValidationEmail('');
-        setErrorValidationPassword('');
+        console.log(555)
+        setConfirm('')
+        setErrorRegister(data.message)
+        setErrorValidationName('')
+        setErrorValidationEmail('')
+        setErrorValidationPassword('')
       }
     }
-  };
+  }
 
   // ******************
 
@@ -94,13 +94,17 @@ const Login = () => {
     let schema = joi.object({
       username: joi
         .string()
-        .pattern(/^[A-Za-z][0-9]{3,30}/)
+        .pattern(/^[A-Za-z]{4,5}[A-Za-z0-9]{0,30}/)
+        .min(3)
+        .max(30)
         .required()
+
         .messages({
-          'string.empty': 'plz enter your name',
+          'string.empty': 'please enter your name',
+          'string.pattern.base':
+            ' name must start with character to 4 characters    ',
           'string.min': 'min length must be at least 3 characters',
           'string.max': 'max length must be at less than 30 characters',
-          'string.pattern.base': 'your name must start one character',
         }),
       email: joi
         .string()
@@ -110,25 +114,25 @@ const Login = () => {
         })
         .required()
         .messages({
-          'string.empty': 'plz enter your email',
-          'string.email': 'plz enter valid email',
+          'string.empty': 'please enter your email',
+          'string.email': 'please enter valid email',
         }),
       password: joi
         .string()
-        .pattern(/^[A-Za-z0-9]{5,30}$/)
+        .pattern(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,30}$/)
         .messages({
-          'string.empty': 'plz enter your password',
+          'string.empty': 'please enter your password',
           'string.pattern.base':
-            'password pattern is A-Z or a-z or 0-9 min length is 5 and max length is 30',
+            'Minimum eight characters, at least one letter, one number and one special character',
         }),
-    });
-    return schema.validate(user, { abortEarly: false });
+    })
+    return schema.validate(user, { abortEarly: false })
   }
 
   return (
     <>
-      <div className="login">
-        <div className="top">
+      <div className='login'>
+        <div className='top'>
           {/* {errorRegister && (
             <p className="top-err alert alert-danger text-center fw-bold fs-5">
               {errorRegister}
@@ -139,12 +143,12 @@ const Login = () => {
               {confirm}
             </p>
           )} */}
-          <div className="wrapper">
-            <Link to="/register">
+          <div className='wrapper'>
+            <Link to='/register'>
               <img
-                className="logo"
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
-                alt=""
+                className='logo'
+                src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png'
+                alt=''
               />
             </Link>
           </div>
@@ -156,28 +160,28 @@ const Login = () => {
               </div>
             ))
           : ''} */}
-        <div className="container justify-content-start">
+        <div className='container justify-content-start'>
           {errorRegister && (
-            <p className="top-err alert alert-danger text-center fw-bold fs-5 w-50">
+            <p className='top-err alert alert-danger text-center fw-bold fs-5 w-50'>
               {errorRegister}
             </p>
           )}
           {confirm && (
-            <p className="top-conf alert alert-success text-center fw-bold fs-5 w-50">
+            <p className='top-conf alert alert-success text-center fw-bold fs-5 w-50'>
               {confirm}
             </p>
           )}
           <form onSubmit={onSubmit}>
             <h1>Register</h1>
             <input
-              type="text"
-              name="username"
-              placeholder="Enter user name"
+              type='text'
+              name='username'
+              placeholder='Enter user name'
               value={username}
               onChange={onChange}
             />
             {errorValidationName ? (
-              <div className="text-warning text-center h5">
+              <div className='text-warning text-center h5'>
                 {errorValidationName}
               </div>
             ) : (
@@ -185,14 +189,14 @@ const Login = () => {
             )}
 
             <input
-              type="email"
-              name="email"
+              type='email'
+              name='email'
               value={email}
-              placeholder="Enter email"
+              placeholder='Enter email'
               onChange={onChange}
             />
             {errorValidationEmail ? (
-              <div className="text-warning text-center h5">
+              <div className='text-warning text-center h5'>
                 {errorValidationEmail}
               </div>
             ) : (
@@ -200,27 +204,27 @@ const Login = () => {
             )}
 
             <input
-              type="password"
-              name="password"
+              type='password'
+              name='password'
               value={password}
-              placeholder="Enter password"
+              placeholder='Enter password'
               onChange={onChange}
             />
             {errorValidationPassword ? (
-              <div className="text-warning text-center h5">
+              <div className='text-warning text-center h5'>
                 {errorValidationPassword}
               </div>
             ) : (
               ''
             )}
 
-            <button className="loginbutton" type="submit">
+            <button className='loginbutton' type='submit'>
               Register
             </button>
 
             <span>
               New To Netflix ?
-              <Link className="signUp" to="/login">
+              <Link className='signUp' to='/login'>
                 <b>Log in Now</b>
               </Link>
             </span>
@@ -233,7 +237,7 @@ const Login = () => {
       </div>
       <Footer />
     </>
-  );
-};
+  )
+}
 
-export default Login;
+export default Login
